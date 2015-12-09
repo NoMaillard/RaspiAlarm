b38cc3eebdbddcc95c2c951cfe6aafcc7c878ded
Noé Maillard <noemaillard@MacBook-Pro-de-Noe.local>
2015-12-09 22:30:10 +0100

    fisrt web interface working

--- alarm.js
+++ alarm.js
@@ -1,5 +1,6 @@
 var util = require('util');
 var EventEmitter = require('events').EventEmitter;
+var exec = require('child_process').exec;
 
 
 function Alarm () {
@@ -20,7 +21,19 @@ function Alarm () {
     * }
     */
     self.alarms = [];
-}
+    self.canPlay = true;
+
+};
+
+var daysOfTheWeek = {
+    "Sunday" : 0,
+    "Monday" : 1,
+    "Tuesday" : 2,
+    "Wednesday" : 3,
+    "Thursday" : 4,
+    "Friday" : 5,
+    "Saturday" : 6
+};
 
 util.inherits(Alarm, EventEmitter);
 
@@ -37,8 +50,8 @@ Alarm.prototype.addAlarm = function(name, hr, min, dow) {
 
     this.alarms[this.alarms.length] = {
         name : name,
-        hr : 10,
-        min : 26,
+        hr : hr,
+        min : min,
         dow : dow
     }
 }
@@ -58,37 +71,84 @@ Alarm.prototype.deleteAlarm = function (name) {
     }
 }
 
+Alarm.prototype.check = function () {
+    var self = this;
+    setInterval(function () {
+        now = new Date();
+        for (alarm of self.alarms) {
+            if (alarm.hr === now.getHours() &&
+            alarm.min === now.getMinutes() &&
+            isIn(now.getDay(), alarm.dow)) {
+                self.emit('ring');
+            }
+        }
+    }, 10000)
+};
+
+Alarm.prototype.stop = function () {
+    this.emit('stop');
+};
+
+Alarm.prototype.ring = function () {
+    this.emit('ring');
+};
+
+/**
+* finds an element in an array copied from jQuery
+* @param  {object}  needle   thing to find
+* @param  {array}  haystack array to find the object in
+* @return {Boolean}          returns true is hte object is in the array
+*/
+var isIn = function (needle, haystack) {
+    var length = haystack.length;
+    for(var i = 0; i < length; i++) {
+        if(haystack[i] == needle) return true;
+    }
+    return false;
+}
+
 // util funtion to check integrity of the daysOfTheWeek array (dow)
 function validate_dow(dow) {
 
-    /**
-    * finds an element in an array copied from jQuery
-    * @param  {object}  needle   thing to find
-    * @param  {array}  haystack array to find the object in
-    * @return {Boolean}          returns true is hte object is in the array
-    */
-    var isIn = function (needle, haystack) {
-        var length = haystack.length;
-        for(var i = 0; i < length; i++) {
-            if(haystack[i] == needle) return true;
-        }
-        return false;
-    }
-
     if (dow === "weekdays") {
-        dow = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-    } else if (dwo === "weekends") {
-        dow = ["Saturday", "Sunday"];
+        dow = [
+            daysOfTheWeek["Monday"],
+            daysOfTheWeek["Tuesday"],
+            daysOfTheWeek["Wednesday"],
+            daysOfTheWeek["Thursday"],
+            daysOfTheWeek["Friday"]
+        ];
+    } else if (dow === "weekends") {
+        dow = [
+            daysOfTheWeek["Saturday"],
+            daysOfTheWeek["Sunday"]
+        ];
     } else {
-        for (var day in dow) {
-            if (!isIn(day, dow)) {
-                throw "The daysOfTheWeek array is Wrong !"
-            }
+        var i = 0;
+        for (var day of dow) {
+            dow[i++] = daysOfTheWeek[day];
         }
     }
     return dow;
 }
 
+var alarm = new Alarm();
+// this is the alarm checker, runs indefinately
+alarm.on('ring', function () {
+    if (alarm.canPlay) {
+        alarm.player = exec('mplayer song.mp3');
+        alarm.canPlay = false;
+    }
+}).on('stop', function () {
+    if (alarm.player) {
+        alarm.player.kill();
+        alarm.canPlay = true;
+    }
 
+}).check();
+
+alarm.addAlarm("wake", 7, 00, ['weekdays']);
+alarm.addAlarm("wake", 7, 05, ['weekdays']);
+alarm.addAlarm("wake", 7, 10, ['weekdays']);
 // expose the module
-module.exports = new Alarm();
+module.exports = alarm
--- app.js
+++
@@ -1,7 +0,0 @@
-var alarm = require('./alarm');
-var EventEmitter = require('events').EventEmitter;
-
-alarm.addAlarm("Wake1", 10, 34, "weekdays");
-alarm.addAlarm("Wake1", 10, 34, "weekdays");
-
-console.log(alarm);
