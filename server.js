3cd0d3b4449d8754eee3b628c45a355c938167a0
Noé Maillard <noemaillard@MacBook-Pro-de-Noe.local>
2015-12-16 00:10:19 +0100

    missing var

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express        = require('express');
 var app            = express();
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
-alarm              = require('./alarm');
+var alarm          = require('./alarm');
 var server         = require('http').createServer(app);
 var io             = require('socket.io')(server);
 
